3442db672d583d03ae8788fad1318a1549eeea1d
semicolon guy <itworksnavneet@gmail.com>
2025-09-24 11:14:33 +0530

    add profile component
    - uses labels for name and bio
    - primevue component: Textarea, InputText and Button for basic styling

---
+++ src/components/Profile.vue
@@ -0,0 +1,33 @@
+<script>
+// import InputText from 'primevue/inputtext';
+import { Textarea, InputText, Button } from 'primevue'
+
+export default {
+  components: {
+    InputText,
+    Textarea,
+    Button
+  },
+  data() {
+    return {
+      name: null,
+      bio: null,
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <label for="name">Enter Name: </label><br>
+    <!-- <input type="text" v-model="name"> -->
+    <InputText id="name" type="text" v-model="name" />
+
+    <br>
+    <label for="bio">Bio:</label><br>
+    <!-- <textarea name="bio" id="bio" v-model="bio"></textarea> -->
+    <Textarea v-model="bio" rows="2" cols="30" />
+    <br>
+    <Button>Click Me</Button>
+  </div>
+</template>
